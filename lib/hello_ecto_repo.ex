63cf97a7681c29dbea7c86f726749174a37b75e6
Christian Fei <crifei93@gmail.com>
2015-10-25 22:34:51 +0100

    added Repo.all_messages

--- lib/hello_ecto_repo.ex
+++ lib/hello_ecto_repo.ex
@@ -1,3 +1,10 @@
 defmodule HelloEcto.Repo do
   use Ecto.Repo, otp_app: :hello_ecto
+  import Ecto.Query
+
+  def all_messages do
+    query = from m in Message,
+         select: m
+    all(query)
+  end
 end
